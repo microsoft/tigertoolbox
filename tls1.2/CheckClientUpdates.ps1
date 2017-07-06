e87f82d3193b7476c9fde9e2d0a653a8f4c4312a
Amit Banerjee <amit.banerjee@microsoft.com>
2017-07-05 20:15:50 -0700

    Correcting quote formatting
    Removing incorrect formatting for quotes

--- tls1.2/CheckClientUpdates.ps1
+++ tls1.2/CheckClientUpdates.ps1
@@ -1,4 +1,4 @@
-# Helper functions to check if TLS 1.2 updates are required
+﻿# Helper functions to check if TLS 1.2 updates are required
 # Script currently supports checking for the following:
 # a. Check if SQL Server Native Client can support TLS 1.2
 # b. Check if Microsoft ODBC Driver for SQL Server can support TLS 1.2
@@ -67,27 +67,27 @@ Function Check-SqlODBC($server)
 #>
 Function Get-InstalledPrograms()
 {
-    $array = @()
+	$array = @()
     
     #Define the variable to hold the location of Currently Installed Programs
-    $UninstallKey=”SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall” 
+    $UninstallKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" 
     #Create an instance of the Registry Object and open the HKLM base key
-    $reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey(‘LocalMachine’, $env:COMPUTERNAME) 
+    $reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine', $env:COMPUTERNAME) 
     #Drill down into the Uninstall key using the OpenSubKey Method
     $regkey=$reg.OpenSubKey($UninstallKey) 
     #Retrieve an array of string that contain all the subkey names
-    $subkeys=$regkey.GetSubKeyNames() 
+    $subkeys=$regkey.GetSubKeyNames()
     #Open each Subkey and use GetValue Method to return the required values for each
-    foreach($key in $subkeys)
+    foreach ($key in $subkeys)
     {
-        $thisKey=$UninstallKey+”\\”+$key 
+        $thisKey=$UninstallKey+"\\"+$key 
         $thisSubKey=$reg.OpenSubKey($thisKey) 
         $obj = New-Object PSObject
-        $obj | Add-Member -MemberType NoteProperty -Name “ComputerName” -Value $env:COMPUTERNAME
-        $obj | Add-Member -MemberType NoteProperty -Name “DisplayName” -Value $($thisSubKey.GetValue(“DisplayName”))
-        $obj | Add-Member -MemberType NoteProperty -Name “DisplayVersion” -Value $($thisSubKey.GetValue(“DisplayVersion”))
-        $obj | Add-Member -MemberType NoteProperty -Name “InstallLocation” -Value $($thisSubKey.GetValue(“InstallLocation”))
-        $obj | Add-Member -MemberType NoteProperty -Name “Publisher” -Value $($thisSubKey.GetValue(“Publisher”))
+        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $env:COMPUTERNAME
+        $obj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $($thisSubKey.GetValue("DisplayName"))
+        $obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($thisSubKey.GetValue("DisplayVersion"))
+        $obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($thisSubKey.GetValue("InstallLocation"))
+        $obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($thisSubKey.GetValue("Publisher"))
         $array += $obj
     } 
     return $array
