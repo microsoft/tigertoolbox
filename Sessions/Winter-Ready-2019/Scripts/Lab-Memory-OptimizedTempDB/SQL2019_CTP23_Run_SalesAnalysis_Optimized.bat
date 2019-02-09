8b3e2e1cba4477430e50c4883f9f2908351737fd
pmasl <pelopes@microsoft.com>
2019-02-09 00:08:32 -0800

    Adjusted Lab folder

--- Sessions/Winter-Ready-2019/Scripts/Lab-Memory-OptimizedTempDB/SQL2019_CTP23_Run_SalesAnalysis_Optimized.bat
+++
@@ -1,5 +0,0 @@
-ECHO OFF
-rd /s /q %temp%\output
-"ostress.exe" -E -S.\SQL2019_CTP23 -dAdventureWorks -Q"EXEC dbo.usp_SalesAnalysis_Optimized" -mstress -quiet -n1 -r1 | FINDSTR "Cantfindthisstring"
-rd /s /q %temp%\output
-"ostress.exe" -E -S.\SQL2019_CTP23 -dAdventureWorks -Q"EXEC dbo.usp_SalesAnalysis_Optimized" -mstress -quiet -n100 -r300 | FINDSTR "QEXEC Starting Creating elapsed"
